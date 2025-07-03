89b0d86ab0a9d12ed1062f67dfdb3f68031f6570
YFliXeR <yousefxzed@gmail.com>
2025-07-03 04:49:43 +0300

    Renamed the greeting tool to better match its job Added a better way for the agent to display the currently available tools to the user Replaced the agent response from a stream to invoke to make the code more simple (the old stream code hurt my brain)

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def calculator(a: float, b: float) -> str:
     return result 
 
 @tool
-def sayhello(name: str) -> str:
+def specialized_greeting(name: str) -> str:
     """Useful for greeting a user"""
     print("Initializing The Specialized Greeting Tool...\n")
     return f"Hello {name}, nice meeting you"
@@ -45,24 +45,23 @@ def web_search(search_query: str) -> str:
 def main():
     model = init_chat_model(model="gemini-2.5-pro", model_provider="google_genai") #Choosing the model
 
-    tools = [calculator, sayhello, web_search, available_tools]
+    tools = [calculator, specialized_greeting, web_search]
     agent_executor = create_react_agent(model, tools) #Initializing the prebuilt agent and giving it the model and tools we want
 
     print("Welcome! I'm your AI assistant. Type 'quit' to exit.")
-    print(f"Type 1 to show the available tools.")
+    print("Available Tools:", *[tool.name for tool in tools])
+    print("-" * 60)
 
     while True:
         user_input = input("\nYou: ").strip() #Using strip to remove any unnecessary white space at the start or end
 
         if user_input.lower() == "quit":
             break
-
-        print("\nAssistant: ", end="") #end is there to remove the new line that python makes by default so the assistant's answer wouldnt be in a new line 
-    
-        for chunk in agent_executor.stream({"messages": [HumanMessage(content=user_input)]}): #Passing the messages to our agent
-            if "agent" in chunk and "messages" in chunk["agent"]: #Is the current chunk a response from the agent? and if there's any message in that particular response
-                for message in chunk["agent"]["messages"]: #Looping through the messages and printing its content
-                    print(message.content, end="")
+        
+        result = agent_executor.invoke(
+            {"messages": [HumanMessage(content=user_input)]}
+        )
+        print("\nAssistant: ", result["messages"][-1].content)
     print()
 
 if __name__ == "__main__": #If this python file is executed directly then only run the main() function
